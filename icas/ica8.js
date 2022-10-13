5173bd53aa402ba68f74957380614c66d1ca8ad4
loiscwang <chwa7906@colorado.edu>
2022-10-12 23:08:37 -0600

    h

---
+++ icas/ica8.js
@@ -0,0 +1,9 @@
+function revealMessage() {
+    document.getElementById ("hiddenMessage").style.display = 'block';
+}
+
+function revealMessage(1) {
+    document.getElementById ("hiddenMessage").style.display = 'block';
+}
+
+alert("hey welcome to lois's ica8 pg. I have no idea how to use JavaScript, so hopfully this is ok-ish.")
