3aec69995ec4f9afb7b9028d49daa42d29cb768b
loiscwang <112105578+loiscwang@users.noreply.github.com>
2022-10-20 20:27:47 -0600

    Delete icas directory

--- icas/ica8.js
+++
@@ -1,6 +0,0 @@
-function revealMessage() {
-    document.getElementById ("hiddenMessage").style.display = 'block';
-}
-
-
-alert("hey welcome to lois's ica8 pg. I have no idea how to use JavaScript, so hopfully this is ok-ish.")
