abb43d6e2914488ffda5ec0c2b1ccd9ca8755e71
loiscwang <chwa7906@colorado.edu>
2022-10-23 21:13:45 -0600

    c

--- gallery-start/main.js
+++ gallery-start/main.js
@@ -5,14 +5,27 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const images = ['pic1.jpg', 'ic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg']
 
 /* Declaring the alternative text for each image file */
+const alts = {
+    'pic1.jpg':'Closeup of a human eye',
+    'pic2.jpg':'b',
+    'pic3.jpg':'c',
+    'pic4.jpg':'d',
+    'pic5.jpg':'e'
+}
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for (const image of images) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', images/$[image]);
+    newImage.setAttribute('alt', alts[images]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', e => {
+        displayedImage.src = e.target.src;
+        displayedImage.alt = e.target.alt;
+      });
+}
 
 /* Wiring up the Darken/Lighten button */
