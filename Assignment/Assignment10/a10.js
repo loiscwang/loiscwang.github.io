c06c04119e6374d705ec6132af4a27f25be0b354
loiscwang <chwa7906@colorado.edu>
2022-10-27 10:35:55 -0600

    Update a10.js

--- Assignment/Assignment10/a10.js
+++ Assignment/Assignment10/a10.js
@@ -1,23 +1,42 @@
-JSON [
+//Question 1//
+[
+    //Question 2//
     {
-        "firstName": "Sam",
-        "department": "Tech",
-        "designation": "Manager", 
-        "salary": "40000", 
-        "raiseEligible": "true",
-    },
-    {
-        "firstName": "Mary",
-        "department": "Finance",
-        "designation": "Trainee", 
-        "salary": "18500", 
-        "raiseEligible": "true",
-    },
-    {
-        "firstName": "Bill",
-        "department": "HR",
-        "designation": "Exective", 
-        "salary": "21200", 
-        "raiseEligible": "false",
+        "companyName": "Tech Stars",
+        "website": "www.techstars.site",
+        "employee": 
+        [
+            {
+                "firstName": "Sam",
+                "department": "Tech",
+                "designation": "Manager", 
+                "salary": "40000", 
+                "raiseEligible": "true",
+            },
+            {
+                "firstName": "Mary",
+                "department": "Finance",
+                "designation": "Trainee", 
+                "salary": "18500", 
+                "raiseEligible": "true",
+            },
+            {
+                "firstName": "Bill",
+                "department": "HR",
+                "designation": "Exective", 
+                "salary": "21200", 
+                "raiseEligible": "false",
+            }, 
+             //Question 3//
+            {
+                "firstName": "Anna",
+                "department": "Tech",
+                "designation": "Exective", 
+                "salary": "25600", 
+                "raiseEligible": "false",
+            }
+        ] 
     }
 ]
+
+console.log ()
