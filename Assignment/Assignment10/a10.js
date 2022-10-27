90762541391e8f82078dec9e33e70f5a1e083440
loiscwang <chwa7906@colorado.edu>
2022-10-27 10:07:04 -0600

    i

---
+++ Assignment/Assignment10/a10.js
@@ -0,0 +1,23 @@
+JSON [
+    {
+        "firstName": "Sam",
+        "department": "Tech",
+        "designation": "Manager", 
+        "salary": "40000", 
+        "raiseEligible": "true",
+    },
+    {
+        "firstName": "Mary",
+        "department": "Finance",
+        "designation": "Trainee", 
+        "salary": "18500", 
+        "raiseEligible": "true",
+    },
+    {
+        "firstName": "Bill",
+        "department": "HR",
+        "designation": "Exective", 
+        "salary": "21200", 
+        "raiseEligible": "false",
+    }
+]
