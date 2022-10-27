c8995916b36fc823a8dba9b36c21e3683b53ad30
loiscwang <chwa7906@colorado.edu>
2022-10-27 10:47:50 -0600

    Update a10.js

--- Assignment/Assignment10/a10.js
+++ Assignment/Assignment10/a10.js
@@ -1,42 +1,77 @@
+
 //Question 1//
 [
-    //Question 2//
+    {
+        "firstName": "Sam",
+        "department": "Tech",
+        "designation": "Manager", 
+        "salary": "40000", 
+        "raiseEligible": "true",
+    },
+    {
+        "firstName": "Mary",
+        "department": "Finance",
+        "designation": "Trainee", 
+        "salary": "18500", 
+        "raiseEligible": "true",
+    },
+    {
+        "firstName": "Bill",
+        "department": "HR",
+        "designation": "Exective", 
+        "salary": "21200", 
+        "raiseEligible": "false",
+    }, 
+    {
+        "firstName": "Anna",
+        "department": "Tech",
+        "designation": "Exective", 
+        "salary": "25600", 
+        "raiseEligible": "false",
+    }
+] 
+
+console.log ()
+
+
+
+
+//Question 2//
+[
     {
         "companyName": "Tech Stars",
         "website": "www.techstars.site",
-        "employee": 
-        [
-            {
-                "firstName": "Sam",
-                "department": "Tech",
-                "designation": "Manager", 
-                "salary": "40000", 
-                "raiseEligible": "true",
-            },
-            {
-                "firstName": "Mary",
-                "department": "Finance",
-                "designation": "Trainee", 
-                "salary": "18500", 
-                "raiseEligible": "true",
-            },
-            {
-                "firstName": "Bill",
-                "department": "HR",
-                "designation": "Exective", 
-                "salary": "21200", 
-                "raiseEligible": "false",
-            }, 
-             //Question 3//
-            {
-                "firstName": "Anna",
-                "department": "Tech",
-                "designation": "Exective", 
-                "salary": "25600", 
-                "raiseEligible": "false",
-            }
-        ] 
+        "employee": []
+    }
+]   
+
+console.log ()
+
+
+
+
+//Question 3//
+[
+    {
+    "firstName": "Anna",
+    "department": "Tech",
+    "designation": "Exective", 
+    "salary": "25600", 
+    "raiseEligible": "false",
     }
 ]
+
+console.log ()
+
+//Question 4//
+
+
+
+
+
+
+
+
+
 
 console.log ()
