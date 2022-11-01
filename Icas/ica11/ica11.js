a0706aea7de72c9ba60c10675703f2c9e0af5d00
loiscwang <chwa7906@colorado.edu>
2022-11-01 10:16:01 -0600

    i

--- Icas/ica11/ica11.js
+++ Icas/ica11/ica11.js
@@ -1,1 +1,10 @@
 const quoteButtoon = document.querySelector('new-quote button')
+quoteButtoon.addEventListener('click', getQuote)
+
+const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
+
+function getQuote () {
+    console.log('test click worked');
+    let text = fetch(endpoint);
+    console.log(text);
+}
