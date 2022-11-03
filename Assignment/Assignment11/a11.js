f9643a12f644a74b5019e64a0a154a5bc53abd36
loiscwang <chwa7906@colorado.edu>
2022-11-03 10:02:05 -0600

    w

---
+++ Assignment/Assignment11/a11.js
@@ -0,0 +1,30 @@
+const quoteButtoon = document.querySelector('.new-quote');
+quoteButtoon.addEventListener('click', getQuote);
+
+const endpoint = 'https://order-pizza-api.herokuapp.com/api/swagger.json';
+
+async function getQuote () {
+    //console.log('test click worked');
+    let text = await fetch(endpoint);
+    let response = await text.text();
+    
+    let json_response = JSON.parse(response);
+    //console.log(json_response);
+    console.log(json_response['message']);
+     //.then(text => text.text());
+
+    displayQuote(json_response['message']);
+}
+
+function displayQuote (x) {
+   // const quoteBox = document.querySelector9('#js-quote-text');
+   // console.log ('dislpay qupte working');
+   //const textMessage = document.createTextNode(x); 
+
+    //quoteBox.appendChild(textMessage); 
+
+
+    document.getElementById('js-quote-text').textContent = x;
+}
+
+getQuote();
