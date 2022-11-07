edef2e9006d5c8bce67096add43c80ddcb77bffc
loiscwang <chwa7906@colorado.edu>
2022-11-06 23:19:23 -0700

    t

--- Assignment/Assignment11/a11.js
+++ Assignment/Assignment11/a11.js
@@ -14,6 +14,18 @@ async function getQuote () {
      //.then(text => text.text());
     displayQuote(json_response['setup']);
 
+    let answerButton = document.querySelector('.tbutton');
+    answerButton.addEventListener('click', getPunchline);
+    
+    function getPunchline() { 
+        console.log(json_response['punchline']);
+        displayAnswer(json_response['punchline'])
+
+    }
+
+    displayAnswer()
+
+
 
 }
 
@@ -28,22 +40,15 @@ function displayQuote (x) {
     document.getElementById('js-quote-text').textContent = x;
 }
 
-//punchline
-//quoteBox.appendChild(['punchline'])
-
-
-async function getPunchline () {
-    //console.log('test click worked');
-    let text = await fetch(endpoint);
-    let response = await text.text();
-    
-    let json_response = JSON.parse(response);
-    console.log(json_response);
-    console.log(json_response['punchline']);
-     //.then(text => text.text());
-    textContent.appendChild(json_response['punchline']);
-}
-
+function displayAnswer (y) {
+    // const quoteBox = document.querySelector9('#js-quote-text');
+    // console.log ('dislpay qupte working');
+    //const textMessage = document.createTextNode(x); 
+ 
+     //quoteBox.appendChild(textMessage); 
+ 
+     document.getElementById('js-answer-text').textContent = y;
+ }
 
 getQuote();
 
