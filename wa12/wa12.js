b7f96e8c3a3f38b9ea3f5227842100a02265626b
loiscwang <chwa7906@colorado.edu>
2022-11-08 10:07:38 -0700

    d

--- wa12/wa12.js
+++ wa12/wa12.js
@@ -1,4 +1,33 @@
-const button = document.querySelector('.button');
+const minusButton = document.querySelector ('.minus-button').addEventListener('click',check);
+const plussButton = document.querySelector ('.plus-button').addEventListener('click',plus);
+const resetButton = document.querySelector ('.reset-button').addEventListener('click',reset);
+const submitButton = document.querySelector ('.submit-button').addEventListener('click',submit);
+
+
+
+const output = document.querySelector('.output'); 
+
+
+function check() {
+    console.log ('test');
+}
+
+function submit () {
+    alert(output.textContent);
+}
+
+function reset () {
+    //const resetValue = 0000000000;
+    output.textContent = '0000000000'; 
+}
+
+function plus () {
+
+output.textContent = output.textContent + 1
+}
+
+
+/* const button = document.querySelector('.button');
 const output = document.querySelector('.output');
 let phone_content = document.querySelector('.phone');
 
@@ -6,4 +35,5 @@ button.addEventListener('click', updateOutput);
 
 function updateOutput() {
     output.textContent = phone_content.value;
-}
+    alert(phone_content.value);
+} */
