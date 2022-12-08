fe42e5d6f9aaa366cea8b9dcdde42e8dd576497a
loiscwang <chwa7906@colorado.edu>
2022-12-07 17:24:12 -0700

    c

--- style.js
+++ style.js
@@ -4,8 +4,10 @@ var speed = 40; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
   if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
+    document.getElementById("index_item_one").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
 }
+
+console.log ()
